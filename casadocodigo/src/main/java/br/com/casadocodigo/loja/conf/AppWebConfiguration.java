f1667c2433583c02a37a7f727c1a73e98d251c4e
paulolirio <paulolirio@n46v>
2019-03-10 16:12:20 -0300

    adicionado tag configuration

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -2,11 +2,13 @@ package br.com.casadocodigo.loja.conf;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import br.com.casadocodigo.loja.controllers.HomeController;
 
+@Configuration
 @EnableWebMvc
 @ComponentScan(basePackageClasses = {HomeController.class})
 public class AppWebConfiguration {
